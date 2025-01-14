2a685f43e4cf2162670048dff121c0d787b95cb2
cyj <cyj@cyjdeMac-mini.local>
2025-01-14 14:13:24 +0800

    v0.2.2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react"
-import {Button} from "@material-ui/core"
+import { useState } from "react"
+import {Button, MenuItem, Select} from "@material-ui/core"
 import "./about.css"
+import { Config } from "../utils/Config";
+import { translate } from "../utils/translate";
 
 export default function About(){
     const [submenu,setSubmenu]=useState("");
+    const [sets,setSets]=useState({lang:Config.lang})
 
     let content=null;
 
@@ -11,7 +14,10 @@ export default function About(){
             content=(
             <div>
                 <p>Hello，您好，欢迎使用本软件，当前的有些功能还尚未完善，暂且请先不要使用这些功能。</p>
-                <p>版本：v0.2.1</p>
+                <p>版本：v0.2.2</p>
+                <p>本软件的主要功能是练习，您可以加载符合格式的JSON或者CSV文件进行单次练习，也可以加载ZIP文件进行课程的添加。</p>
+                <p>作为课程的压缩文件，由若干个单次练习构成，您可以选择这些练习进行练习。</p>
+                <p>在本软件中，您也可以制作练习文件，当然，也可以在外部编辑器中制作练习。</p>
                 <p></p>
             </div>)
         }else if(submenu=="credits"){
@@ -19,9 +25,26 @@ export default function About(){
         }else if(submenu=="license"){
             content=<div><p>GPL</p></div>
         }else{}
+    
+    const settings=
+        <div>
+            <div>
+                <span>{translate("sets_lang")}{translate("sign_colon")}</span>
+                <Select value={sets.lang} onChange={e=>{
+                    setSets({...sets,lang:e.target.value});
+                    Config.lang=e.target.value;
+                    Config.save();
+                }}>
+                    <MenuItem value="en">English</MenuItem>
+                    <MenuItem value="zh">中文 Chinese</MenuItem>
+                    <MenuItem value="jp">日本語　Japanese</MenuItem>
+                </Select>
+            </div>
+        </div>
 
     return (<div>
         <p>关于</p>
+        {settings}
         <Button className="ab" variant="contained" onClick={()=>setSubmenu("help")}>帮助</Button>
         <Button className="ab" variant="contained" onClick={()=>setSubmenu("credits")}>致谢</Button>
         <Button className="ab" variant="contained" onClick={()=>setSubmenu("license")}>许可协议</Button>
---
+++ src/utils/ConvertCSV.js
@@ -0,0 +1,109 @@
+export function ConvertCSV(csvtxt){
+    // 传入CSV格式字符串，并返回JSON
+    const lines=csvtxt.split("\n");
+    const result={}; 
+    
+    // basic info
+    const n=normal(lines[0].split(","));
+    result.title=n[0]; // 标题
+    if(n[1]!=undefined){
+        // 课程
+        result.course=n[1];
+    }
+    if(n[2]!=undefined){
+        // 章节
+        result.chapter=n[2];
+    }
+    if(n[3]!=undefined){
+        // 作者
+        result.authors=n[3].split(";");
+    }
+    if(n[4]!=undefined){
+        // 许可协议
+        result.licenses=n[4].split(";");
+    }
+    if(n[5]!=undefined){
+        // 简介
+        result.description=n[5];
+    }
+
+    // 
+    let questions=[];
+    let meta={};
+    for(let i=1;i<lines.length;i++){
+        // 从第二行开始每一行,lines[i]代表一行整体字符串
+        const line=normal(lines[i].split(",")); // 一行字段数组
+        let question={};
+
+        if(line[0]==""){
+            // 空行忽略
+            continue;
+        }
+
+        if(line[0]=="0" || line[0]=="choice" || line[0]=="单选"){
+            // 单选
+            question.type="choice";
+            question.title=line[1];
+            question.answer=parseOptionId(line[2]);
+            question.explain=line[3];
+            question.options=line.filter((opt,i)=>i>3);
+        }else if(line[0]=="1" || line[0]=="mchoice" || line[0]=="多选"){
+            // 多选
+            question.type="mchoice";
+            question.title=line[1];
+            question.answer=line[2].split(";").map(opt=>parseOptionId(opt)).sort((a,b)=>a-b);
+            question.explain=line[3];
+            question.options=line.filter((opt,i)=>i>3);
+        }else if(line[0]=="2" || line[0]=="blank" || line[0]=="填空"){
+            // 客观填空
+            question.type="blank";
+            question.title=line[1];
+            question.answer=line[2].split(";")
+            question.explain=line[3];
+        }else{
+            // 视为元数据
+            meta[line[0]]={}
+            meta[line[0]].type=line[1];
+            meta[line[0]].content=line[2];
+        }
+
+        questions.push(question)
+    }
+
+    result.questions=questions;
+    result.meta=meta;
+
+    //console.log(JSON.stringify(result));
+
+    return result;
+}
+
+function normal(line){
+    for(let i=0;i<line.length;i++){
+        if(line[i]==undefined){
+            continue;
+        }
+        if(line[i].startsWith('"')){
+            let count=1;
+            line[i]=line[i].substr(1,line[i].length-1)+",";
+            while(!line[i+count].endsWith('"')){
+                line[i]+=line[i+count];
+                line[i]+=","
+                line[i+count]=undefined;
+            }
+            line[i]+=line[i+count].substr(0,line[i+count].length-1);
+            line[i+count]=undefined;
+        }
+    }
+
+    return line.filter(field=>field!=undefined);
+}
+
+function parseOptionId(opt){
+    // opt必须为单个选项编号字符串，转换为整数
+    if(!isNaN(opt)){
+        return parseInt(opt);
+    }else{
+        return opt.codePointAt(0)-65;
+    }
+}
